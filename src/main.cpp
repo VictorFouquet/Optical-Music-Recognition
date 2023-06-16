4bd4624fec83fb9e4cd39edacd2933dc299db966
victor <victor.fouquet@gmail.com>
2023-06-16 22:41:48 +0200

    Update main entry point

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,47 @@
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <vector>
 
-int main()
+#include "omr.h"
+
+
+int main(int argc, char* argv[])
 {
+    if (argc < 4)
+    {
+        std::cerr
+            << "Please provide [width] [height] and [rle_string] as argument to the program"
+            << std::endl;
+        return 1;
+    }
+
+    int width = std::stoi(argv[1]);
+    int height = std::stoi(argv[2]);
+
+    auto stringstream = std::ostringstream{};
+    std::ifstream rle_file(argv[3]);
+    if (rle_file.is_open())
+    {
+        stringstream << rle_file.rdbuf();
+    }
+
+    std::string rle = stringstream.str();
+
+    OpticalMusicRecognition omr = OpticalMusicRecognition(width, height);
+    omr.read_sheet(rle);
+    std::vector<std::string> notes = omr.get_notes();
+
+    std::string output = "";
+    for (int i = 0; i < notes.size(); i++)
+    {
+        output += notes[i];
+        if (i < notes.size() - 1)
+            output += " ";
+    }
+    std::cout << "Notes contained in the provided image:" << std::endl;
+    std::cout << output << std::endl;
+
     return 0;
 }
